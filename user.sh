aea4611b2e00e6597045c172fac9e097fb0e4877
Meherun Mithila <mnm19322@email.vccs.edu>
2025-02-25 23:34:39 -0500

    7th change after deploying

--- user.sh
+++ user.sh
@@ -3,13 +3,15 @@ component=user
 destination=/tmp/user.zip
 url=https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
 
+cp user.service /etc/systemd/system/user.service
+
 print_head Install nodejs 20
 configure_and_install_nodejs &>> $log_file
 
 print_head Configure and install component
 configure_component &>> $log_file
 
-cp user.service /etc/systemd/system/user.service
+
 
 print_head Reload daemon
 reload_daemon &>> $log_file
