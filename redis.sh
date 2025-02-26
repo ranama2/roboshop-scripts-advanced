a268b941e3d38514395482650b427f18afd885fb
Meherun Mithila <mnm19322@email.vccs.edu>
2025-02-25 23:24:07 -0500

    fifth change after deploying

--- redis.sh
+++ redis.sh
@@ -5,8 +5,8 @@ component_version=redis:7
 print_head Configure and install redis
 configure_and_install_component &>> $log_file
 
-sed -i ‘s|127.0.0.1|0.0.0.0|’ /etc/redis/redis.conf
-sed -i ‘s|protected-mode|protected-mode no|’ /etc/redis/redis.conf
+sed -i 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
+sed -i 's|protected-mode|protected-mode no|' /etc/redis/redis.conf
 
 print_head Enable and start redis
 enable_and_start_component
