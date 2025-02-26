59515e6b943b4b1a6c18caf8fcfa0cceeeac98a6
Meherun Mithila <mnm19322@email.vccs.edu>
2025-02-25 23:38:07 -0500

    8th change after deploying

--- cart.sh
+++ cart.sh
@@ -3,13 +3,15 @@ component=cart
 destination=/tmp/cart.zip
 url=https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 
+cp cart.service /etc/systemd/system/cart.service
+
 print_head Install nodejs 20
 configure_and_install_nodejs &>> $log_file
 
 print_head Configure and install component
 configure_component &>> $log_file
 
-cp cart.service /etc/systemd/system/cart.service
+
 
 print_head Reload daemon
 reload_daemon &>> $log_file
