4677ef89867f4fe6fdd5b85b84befbbafd6ec02b
Meherun Mithila <mnm19322@email.vccs.edu>
2025-02-25 22:58:12 -0500

    second change after deploying

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,8 @@ source common.sh
 component=nginx
 component_version=nginx:1.24
 
+cp nginx.conf /etc/nginx/nginx.conf
+
 print_head Install and start nginx
 configure_and_install_component &>> $log_file
 enable_and_start_component &>> $log_file
@@ -15,7 +17,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp nginx.conf /etc/nginx/nginx.conf
+
 
 print_head Restart nginx
 restart_component &>> $log_file
